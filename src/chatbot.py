590939ad11347d14b28c716a38779ee576723528
Evin Li <evinywli@gmail.com>
2023-09-16 19:47:03 -0700

    added data validation that chatbot input msg isn't null

--- src/chatbot.py
+++ src/chatbot.py
@@ -3,6 +3,8 @@ import cohere
 co = cohere.Client('81BT2ZiL7xz0oLKvb3NqzuG9xcCi5ERtAHM4EUtu')
 
 def get_response(msg, chat_history):
+    if (msg is None):
+        return "Please enter a message."
     response = co.chat(msg,
                         model="command",
                         temperature=0.3,
